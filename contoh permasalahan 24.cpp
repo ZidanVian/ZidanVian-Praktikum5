fd077669df07a6006c4a495c973e2a2db114dea3
ZidanVian <90166800+ZidanVian@users.noreply.github.com>
2021-11-14 09:55:29 +0700

    Add files via upload

---
+++ contoh permasalahan 24.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+
+void garis();
+int main(){
+	garis();
+	printf(" NO	NIM 	NAMA	NILAI\n");
+	garis();
+	getch();
+}
+void garis(){
+	printf ("===============================\n");
+}
